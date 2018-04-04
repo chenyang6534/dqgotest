e3aa14d219c22e4751dfe109563910b82d8b8c96
chenyang <357328436@qq.com>
2018-04-04 17:02:09 +0800

    微信wss

--- src/dq/network/ws_server.go
+++ src/dq/network/ws_server.go
@@ -2,18 +2,20 @@ package network
 
 import (
 	//"time"
-	"net/http"
 	"dq/log"
+	"net/http"
 	//"net"
 	//"sync"
 	//"time"
 	//"fmt"
 	"dq/utils"
+
 	"github.com/gorilla/websocket"
 )
+
 type WSServer struct {
 	ServerData
-	conns           map[*websocket.Conn]struct{}
+	conns map[*websocket.Conn]struct{}
 }
 
 func (server *WSServer) Start() {
@@ -21,13 +23,12 @@ func (server *WSServer) Start() {
 	go server.run()
 }
 
-func (server *WSServer) GetLoginedConnect() *utils.BeeMap{
+func (server *WSServer) GetLoginedConnect() *utils.BeeMap {
 	return server.LoginedConnect
 }
-func (server *WSServer) GetAgents() *utils.BeeMap{
+func (server *WSServer) GetAgents() *utils.BeeMap {
 	return server.Agents
 }
-
 
 var upgrader = websocket.Upgrader{
 	//ReadBufferSize:    4096,
@@ -38,15 +39,15 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func (server *WSServer)SvrConnHandler(w http.ResponseWriter, r *http.Request) {
-	
+func (server *WSServer) SvrConnHandler(w http.ResponseWriter, r *http.Request) {
+
 	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil{
-		log.Info("Upgrade err",err)
+	if err != nil {
+		log.Info("Upgrade err", err)
 		return
 	}
 	log.Info("SvrConnHandler")
-    server.mutexConns.Lock()
+	server.mutexConns.Lock()
 	if len(server.conns) >= server.MaxConnNum {
 		server.mutexConns.Unlock()
 		conn.Close()
@@ -54,42 +55,49 @@ func (server *WSServer)SvrConnHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	server.conns[conn] = struct{}{}
-	
+
 	server.mutexConns.Unlock()
 
 	server.wgConns.Add(1)
 
-	tcpConn := newWSConn(conn, server.PendingWriteNum,server.msgParser)
+	tcpConn := newWSConn(conn, server.PendingWriteNum, server.msgParser)
 	agent := server.NewAgent(tcpConn)
-	
+
 	//server.mutexConns.Lock()
-	server.Agents.Set(agent.GetConnectId(),agent)
-	
+	server.Agents.Set(agent.GetConnectId(), agent)
+
 	//server.mutexConns.Unlock()
 	//time.Sleep(10*time.Second)
 	//go func() {
-		agent.Run()
+	agent.Run()
 
-		// cleanup
-		tcpConn.Close()
-		server.mutexConns.Lock()
-		delete(server.conns, conn)
-		//delete(server.Agents, agent.GetCreateId())
-		server.mutexConns.Unlock()
-		//server.Agents.Delete(agent.GetConnectId())
-		agent.OnClose()
+	// cleanup
+	tcpConn.Close()
+	server.mutexConns.Lock()
+	delete(server.conns, conn)
+	//delete(server.Agents, agent.GetCreateId())
+	server.mutexConns.Unlock()
+	//server.Agents.Delete(agent.GetConnectId())
+	agent.OnClose()
 
-		server.wgConns.Done()
+	server.wgConns.Done()
 	//}()
-    
+
+}
+
+func (server *WSServer) SvrConnHandler1(w http.ResponseWriter, r *http.Request) {
+
+	w.Write([]byte("hello"))
+
 }
 
 func (server *WSServer) init() {
-	http.HandleFunc("/connect",server.SvrConnHandler);
+	http.HandleFunc("/connect", server.SvrConnHandler)
+	http.HandleFunc("/connect1", server.SvrConnHandler1)
 	//ln, err := net.Listen("tcp", server.Addr)
-//	if err != nil {
-//		log.Error("%v", err)
-//	}
+	//	if err != nil {
+	//		log.Error("%v", err)
+	//	}
 
 	if server.MaxConnNum <= 0 {
 		server.MaxConnNum = 100
@@ -111,26 +119,26 @@ func (server *WSServer) init() {
 
 	// msg parser
 	msgParser := NewMsgParser()
-	
+
 	server.msgParser = msgParser
-	
-	log.Info("------Listen:"+server.Addr)
+
+	log.Info("------Listen:" + server.Addr)
 }
 
 func (server *WSServer) run() {
 	server.wgLn.Add(1)
 	defer server.wgLn.Done()
-
+	//err := http.ListenAndServeTLS(server.Addr, "bin/conf/214571202380020.pem", "bin/conf/214571202380020.key", nil)
 	err := http.ListenAndServe(server.Addr, nil)
-    //checkErr(err, "ListenAndServe");
-	if err != nil{
+	//checkErr(err, "ListenAndServe");
+	if err != nil {
 		log.Info(err.Error())
 	}
-    log.Info("Func finish.");
+	log.Info("Func finish.")
 }
 
 func (server *WSServer) Close() {
-	
+
 	//server.ln.Close()
 	server.wgLn.Wait()
 
@@ -141,7 +149,7 @@ func (server *WSServer) Close() {
 	server.conns = nil
 	server.mutexConns.Unlock()
 	//server.Agents = nil
-	
+
 	server.wgConns.Wait()
-	log.Info("tcp Close :%s",server.Addr)
+	log.Info("tcp Close :%s", server.Addr)
 }
